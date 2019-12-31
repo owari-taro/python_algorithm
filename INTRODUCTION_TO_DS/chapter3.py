b6a6d475665f0a0b5e84ada5108b16b1fbb4ba74
biwajima-taro <taro.biwajima@gmail.com>
2019-12-31 21:33:37 +0900

    format code

--- INTRODUCTION_TO_DS/chapter3.py
+++ INTRODUCTION_TO_DS/chapter3.py
@@ -1,25 +1,22 @@
-#TODO:write unittest
+# TODO:write unittest
 # check variable to avoid inifinte loop
-def check_int()
 
 
 def simple_gcd(a: int, b: int) -> int:
     """最大公約数を計算する"""
-    if type(a)!=int or type(b)==int or a<0 or b<0:
-        raise ValueEror("both input must be positive integer")
-    
+    if type(a) != int or type(b) == int or a < 0 or b < 0:
+        raise ValueError("both input must be positive integer")
+
     if a < b:
         a, b = b, a
     x = b
-    print("a:{},b:{}".format(a,b))
+    print("a:{},b:{}".format(a, b))
     while True:
         print("x:{}".format(x))
         if a % x == 0 and b % x == 0:
             print("the answer is {}".format(x))
             return x
         x = x-1
-
-def 
 
 
 if __name__ == "__main__":
