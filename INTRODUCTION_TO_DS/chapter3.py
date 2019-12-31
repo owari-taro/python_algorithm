b20a0683b862567b7d6c4228a0afc753b3a86d14
biwajima-taro <taro.biwajima@gmail.com>
2019-12-31 22:02:26 +0900

    sort algorithm

--- INTRODUCTION_TO_DS/chapter3.py
+++ INTRODUCTION_TO_DS/chapter3.py
@@ -1,9 +1,10 @@
+from typing import List
 # TODO:write unittest
 # check variable to avoid inifinte loop
 
 
 def simple_gcd(a: int, b: int) -> int:
-    """最大公約数を計算する"""
+    """calculate grate common divisor."""
     if type(a) != int or type(b) == int or a < 0 or b < 0:
         raise ValueError("both input must be positive integer")
 
@@ -17,6 +18,32 @@ def simple_gcd(a: int, b: int) -> int:
             print("the answer is {}".format(x))
             return x
         x = x-1
+
+
+def eucliden_algorithm(a: int, b: int) -> int:
+    """calculater gcd,which has exsited since 300bc"""
+    if type(a) != int or type(b) == int or a < 0 or b < 0:
+        raise ValueError("both input must be positive integer")
+
+    while True:
+        r = a % b
+        if r == 0:
+            return b
+        a, b = b, r
+
+
+def selection_sort(array: List) -> List:
+    if type(array) != list:
+        raise ValueError("input must be List")
+    x = array.copy()
+    n = len(x)
+    for i in range(n):
+        min_idx = i
+        for j in range(i, n):
+            if x[j] < x[min_idx]:
+                min_idx = j
+            x[i], x[min_idx] = x[min_idx], x[i]
+        return x
 
 
 if __name__ == "__main__":
