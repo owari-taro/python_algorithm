1cf747858bb454479d2773d5af438b28c7386c91
biwajima-taro <taro.biwajima@gmail.com>
2020-01-04 23:05:00 +0900

    fix typo at knapsack

--- INTRODUCTION_TO_DS/chapter7.py
+++ INTRODUCTION_TO_DS/chapter7.py
@@ -11,7 +11,7 @@ class Knapsack:
         self.value = 0
         self.items = []
 
-    def append(self, item: Item):
+    def append(self, item: Item) -> None:
         if not self.has_room_for(item):
             raise ValueError("too many items,over weight!!")
         self.items.append(item)
@@ -26,9 +26,10 @@ class Knapsack:
         val = f"weight:{self.weight}kg value:\\{self.value}thousand yen"
         return val
 
-def greedy(items:List[Item],size_limit:int):
-    sorted_item_list=sorted(items,key:lamda x:x.price/x.weight,reverse=True)
-    my_knapsack=Knapsack(size_limit)
+
+def greedy(items: List[Item], size_limit: int) -> Knapsack:
+    sorted_item_list = sorted(items, key=lamda x: x.price/x.weight, reverse=True)
+    my_knapsack = Knapsack(size_limit)
     for v in sorted_items_list:
         try:
             my_knapsack.append(v)
