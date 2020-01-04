1d428fbc685e363f6126289054d831b6e1adeb87
biwajima-taro <taro.biwajima@gmail.com>
2020-01-04 21:15:50 +0900

    bread search first for graph

--- INTRODUCTION_TO_DS/chapter6.py
+++ INTRODUCTION_TO_DS/chapter6.py
@@ -1,13 +1,15 @@
 import random
 from typing import List, Set, Tuple
+from collections import deque
 
 
 def generate_graph(n: int, m: int) -> Tuple[List, Set]:
-    ""
-    if m>(n**2-n)/2:
-        raise ValueError(f"m is too big!m must be equtl or less than (n*2-n)/2.")
+    """generate graph search"""
+    if m > (n**2-n)/2:
+        raise ValueError(
+            f"m is too big!m must be equtl or less than (n*2-n)/2.")
     graph_data: List[int] = [[0]*n for i in range(n)]
-    edge_set:Set[Tuple[int,int]] = set()
+    edge_set: Set[Tuple[int, int]] = set()
     while len(edge_set) < m:
         i, j = random.sample(range(n), 2)
         if i > j:
@@ -17,8 +19,25 @@ def generate_graph(n: int, m: int) -> Tuple[List, Set]:
     return graph_data, edge_set
 
 
+def breadth_first_search(start: int, W: List[List[int]]) -> Set:
+    work_queue: deque = deque([])
+    visited: Set[int] = set()
+    work_queue.append(start)
+    visited.add(start)
+    while work_queue:
+        here = work_queue.popleft()
+        for i, node in enumerate(W[here]):
+            if node == 0:
+                continue
+            if i not in visited:
+                visited.add(i)
+                work_queue.append(i)
+    return visited
+
+
 if __name__ == "__main__":
     n = 150
-    m = 20
+    m = 30
     graph, edge = generate_graph(n, m)
-    print(edge)
+    print(breadth_first_search(3,graph))
+    
