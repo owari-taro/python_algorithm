8713fa5bce36bbc2a89d41d7497ae1c6226db2c2
biwajima-taro <taro.biwajima@gmail.com>
2020-01-11 23:16:53 +0900

    fibonach

--- introducion_to_algorithm/dynamic_programing.py
+++ introducion_to_algorithm/dynamic_programing.py
@@ -23,3 +23,19 @@ def bottom_up_cut_rod(p: List, n: int) -> float:
             q = max(q, p[i]+r[j-i])
         r[j] = q
     return q
+
+def fibonach(n: int) -> int:
+    """problem15.1-5"""
+    if n < 0:
+        raise ValueError("n must be positive")
+    f: List[int] = [0, 1]
+    if n == 0 or n == 1:
+        return f[n]
+    for i in range(2, n):
+        f.append(f[i-1]+f[i-2])
+
+    return f[-1]
+
+if __name__=="__main__":
+    prnt("aaaas")
+    print(fibonach(1000))    
