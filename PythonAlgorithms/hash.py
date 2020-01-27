7443826c1b7cf8c9c8b66a6d3b81e1c2a3ad2932
taro-biwajima <taro.biwajima@gmail.com>
2020-01-27 21:11:01 +0900

    dump for hash table

--- PythonAlgorithms/hash.py
+++ PythonAlgorithms/hash.py
@@ -42,19 +42,26 @@ class ChainHash:
         self.table[hash] = tmp
         return True
 
-
-    def remove(sefl,key:Any)->bool:
-        hash=self.hash(key)
-        p=self.table[hash]
-        pp=None
+    def remove(sefl, key: Any) -> bool:
+        hash = self.hash(key)
+        p = self.table[hash]
+        pp = None
 
         while p is not None:
-            if p.key=key:
+            if p.key = key:
                 if pp is None:
-                    self.table[hash]=p.next
+                    self.table[hash] = p.next
                 else:
-                    pp.next=p.next
+                    pp.next = p.next
                 return True
-            pp=p.next
+            pp = p.next
         return False
 
+    def dump(self)->None:
+        for i in range(self.capacity):
+            p=self.table[i]
+            print(i,end="")
+            while p is not None:
+                print()
+                p=p.next
+            print()
