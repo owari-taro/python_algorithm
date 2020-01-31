4cd2a1cf66a40ae148dd5af135601c7b195d83de
biwajima-taro <taro.biwajima@gmail.com>
2020-01-31 17:24:26 +0900

    fix typo at Null and reformat codes

--- PythonAlgorithms/array_linked_list.py
+++ PythonAlgorithms/array_linked_list.py
@@ -1,35 +1,37 @@
 from typing import Any
-Null=-1
+Null = -1
+
+
 class Node:
 
-    def __init__(self,data=Null,next=null,dnext=null):
-        self.data=data
-        self.next=next
-        self.dnext=dnext
-    
+    def __init__(self, data=Null, next=Null, dnext=Null):
+        self.data = data
+        self.next = next
+        self.dnext = dnext
+
 
 class ArrayLinkedList:
 
-    def __init__(self,capacity:int):
-        self.head=Null
-        self.current=Null
-        self.max=Null
-        self.deleted=Null
-        self.capacity=capacity
-        self.n=[Node()]*self.capacity
-        self.no=0
+    def __init__(self, capacity: int):
+        self.head = Null
+        self.current = Null
+        self.max = Null
+        self.deleted = Null
+        self.capacity = capacity
+        self.n = [Node()]*self.capacity
+        self.no = 0
 
-    def __len__(self)->int:
+    def __len__(self) -> int:
         return self.no
-    
+
     def get_insert_index(self):
-        if self.deleted==Null:
-            if self.max<self.capacity:
-                self.max+=1
+        if self.deleted == Null:
+            if self.max < self.capacity:
+                self.max += 1
                 return self.max
             else:
                 return Null
         else:
-            rec=self.deleted
-            self.deleted=self.n[rec].dnext
+            rec = self.deleted
+            self.deleted = self.n[rec].dnext
             return rec
