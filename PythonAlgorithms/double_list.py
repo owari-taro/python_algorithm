5e1f0adf756b0debb0ff1c1b7ddd14cc1411dac7
taro-biwajima <taro.biwajima@gmail.com>
2020-02-01 23:01:57 +0900

    seach

--- PythonAlgorithms/double_list.py
+++ PythonAlgorithms/double_list.py
@@ -1,20 +1,31 @@
 from typing import Any
 
+
 class Node:
-    def __init__(self,data:Any=None,prev*Node=None,next:Node=None):
-        self.data=data
-        self.prev=prev or self
-        self.next=next or self
-    
-    
+    def __init__(self, data: Any = None, prev*Node=None, next: Node = None):
+        self.data = data
+        self.prev = prev or self
+        self.next = next or self
+
+
 class DoubleLinkedList:
-    def __init__(self)->None:
-        self.head=self.current=Node()
-        self. no=0
-    
-    def __len__(self)->int:
+    def __init__(self) -> None:
+        self.head = self.current = Node()
+        self. no = 0
+
+    def __len__(self) -> int:
         return self.no
-    
-    def is_empty(self)->bool:   
+
+    def is_empty(self) -> bool:
         return self.head.next is self.head
-    
+
+    def search(self, data: Any) -> Any:
+        cnt = 0
+        ptr = self.head.next
+        while ptr is not self.head:
+            if data == ptr.data:
+                self.current = ptr
+                return cnt
+            cnt += 1
+            ptr = ptr.next
+        reeturn - 1
