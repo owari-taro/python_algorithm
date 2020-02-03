4ae7d6a0eea5960f99de5fcc2c1c633787e751ad
biwajima-taro <taro.biwajima@gmail.com>
2020-02-03 16:53:40 +0900

    enque deque

--- PythonAlgorithms/fixed_queue.py
+++ PythonAlgorithms/fixed_queue.py
@@ -18,5 +18,27 @@ class FixedQueue:
     def __len__(self) -> int:
         return self.no
 
-    def is_empty(self)->bool:
-        return self.no<=0
+    def is_empty(self) -> bool:
+        return self.no <= 0
+
+    def is_full(self) -> bool:
+        return self.no >= self.capacity
+
+    def enque(self, x: Any) -> None:
+        if self.is_full():
+            raise FixedQueue.Full
+        self.que[self.rear] = x
+        self.rear += 1
+        self.no += 1
+        if self.rear == self.capacity:
+            self.rear = 0
+
+    def deque(self) -> Any:
+        if self.is_empty():
+            raise FixedQueue.Empty
+        x = self.que[self.front]
+        self.front += 1
+        self.no -= 1
+        if self.front == self.capacity:
+            self.front = 0
+        return x
