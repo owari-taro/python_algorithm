c0837bae04f1bb44b3ca69e5fd72d51aed446adf
biwajima-taro <taro.biwajima@gmail.com>
2020-02-04 15:03:33 +0900

    remove

--- PythonAlgorithms/bst.py
+++ PythonAlgorithms/bst.py
@@ -30,7 +30,9 @@ class BinarySearchTree:
 
     def add(self, key: Any, value: Any) -> bool:
         def add_node(node: Node, key: Any, value: Any) -> None:
+            """"helper function for add""""
             if key == node.key:
+                # key is already saved
                 return False
             elif key < node.key:
                 if node.left is None:
@@ -43,8 +45,36 @@ class BinarySearchTree:
                 else:
                     add_node(node.right, key, value)
             return True
+
         if self.root is None:
             self.root = Node(key, value, None, None)
             return True
         else:
             return add_node(self.root, key, value)
+
+    def remove(self, key: Any) -> bool:
+        p = self.root
+        parent = None
+        is_left_child = True
+
+        while True if p is None:
+            if p is None:
+                return False
+            if key == p.key:
+                break
+        else:
+            parent = p
+            if key < p.key:
+                is _left_child = True
+                p = p.left
+            else:
+                is_left_child = False
+                p = p.right
+
+        if p.left is None:
+            if p is self.root:
+                slef.root = p.right
+            elif is_left_child:
+                parent.left = p.right
+            else:
+                parent.right = p.right
