8f8c38a60bcf90565fb64c3ead76a85cccce4b74
biwajima-taro <taro.biwajima@gmail.com>
2020-02-04 16:08:35 +0900

    fix some typo

--- PythonAlgorithms/bst.py
+++ PythonAlgorithms/bst.py
@@ -13,7 +13,7 @@ class Node:
 class BinarySearchTree:
 
     def __init__(self):
-        #
+        # root is none at first
         self.root = None
 
     def search(self, key: Any) -> Any:
@@ -29,8 +29,9 @@ class BinarySearchTree:
                 p = p.right
 
     def add(self, key: Any, value: Any) -> bool:
+
         def add_node(node: Node, key: Any, value: Any) -> None:
-            """"helper function for add""""
+            """helper function for add method"""
             if key == node.key:
                 # key is already saved
                 return False
@@ -57,7 +58,7 @@ class BinarySearchTree:
         parent = None
         is_left_child = True
 
-        while True if p is None:
+        while True:
             if p is None:
                 return False
             if key == p.key:
@@ -65,7 +66,7 @@ class BinarySearchTree:
         else:
             parent = p
             if key < p.key:
-                is _left_child = True
+                is_left_child = True
                 p = p.left
             else:
                 is_left_child = False
@@ -73,8 +74,21 @@ class BinarySearchTree:
 
         if p.left is None:
             if p is self.root:
-                slef.root = p.right
+                self.root = p.right
             elif is_left_child:
                 parent.left = p.right
             else:
                 parent.right = p.right
+        elif p.right is None:
+            if p is self.root:
+                self.root = p._left
+            elif is_left_child:
+                parent.left = p.left
+            else:
+                parent.right = p.left
+        else:
+            parent = p
+            left = p.left
+            is_left_child = True
+            while left.right is None:
+                print()
