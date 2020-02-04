af8ac969f186ce8ac3f9e1e72527c577b2c75d92
biwajima-taro <taro.biwajima@gmail.com>
2020-02-04 14:58:12 +0900

    add method for binary tree

--- PythonAlgorithms/bst.py
+++ PythonAlgorithms/bst.py
@@ -27,3 +27,24 @@ class BinarySearchTree:
                 p = p.left
             else:
                 p = p.right
+
+    def add(self, key: Any, value: Any) -> bool:
+        def add_node(node: Node, key: Any, value: Any) -> None:
+            if key == node.key:
+                return False
+            elif key < node.key:
+                if node.left is None:
+                    node.left = Node(key, value, None, None)
+                else:
+                    add_node(node.left, key, value)
+            else:
+                if node.right is None:
+                    node.rihgt = Node(key, value, None, None)
+                else:
+                    add_node(node.right, key, value)
+            return True
+        if self.root is None:
+            self.root = Node(key, value, None, None)
+            return True
+        else:
+            return add_node(self.root, key, value)
