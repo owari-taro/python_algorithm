ebfbab8520b7ae86a73c158283b86df1baaacdf4
biwajima-taro <taro.biwajima@gmail.com>
2020-02-05 10:57:47 +0900

    flipflop

---
+++ ComputerScienceAnOverview/gate.py
@@ -0,0 +1,27 @@
+
+
+def AND(x:int,y:int):
+    if x and y:
+        return 1
+    else:
+        return 0
+
+def OR(x:int,y:int):
+    if x or y:
+        return 1
+    else:
+        return 0
+
+def NOT(x:int):
+    if x:
+        return 0
+    else:
+        reurn 1
+
+def XOR(x:int,y:int):
+    if x ==y:
+        return 0
+    else:
+        return 1
+
+
