4bc58be5d03751e7841df708f67f5d06fcb8013e
biwajima-taro <taro.biwajima@gmail.com>
2020-02-06 13:26:54 +0900

    stack

--- introducion_to_algorithm/chapter10/stack.py
+++ introducion_to_algorithm/chapter10/stack.py
@@ -12,9 +12,12 @@ class Stack:
         self.size_limit = size_limit
         self.stack = [None]*size_limit
         self.head = 0
+        # countting how many elmeents exist in stack
+        # count field make is_full,is_empty much simpler
+        self.count = 0
 
-    def is_full(self) -> None:
-        if self.head == self.size_limit:
+    def is_full(self) -> bool:
+        if self.count >= self.size_limit:
             return True
         else:
             return False
@@ -24,9 +27,10 @@ class Stack:
             raise Stack.OverFlow
         self.stack[self.head] = x
         self.head += 1
+        self.count += 1
 
     def is_empty(self) -> None:
-        if self.head == 0:
+        if self.count == 0:
             return True
         else:
             return False
@@ -36,4 +40,5 @@ class Stack:
             raise Stack.UnderFlow
         else:
             self.head -= 1
+            self.count-=1
             return self.stack[self.head]
