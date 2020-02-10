c995c6a02d336b76294c0ba6c08c5747159ac555
biwajima-taro <taro.biwajima@gmail.com>
2020-02-10 11:40:45 +0900

    add method

--- introducion_to_algorithm/chapter12/binary_search_tree.py
+++ introducion_to_algorithm/chapter12/binary_search_tree.py
@@ -11,8 +11,31 @@ class BinarySearchTree:
     def __init__(self):
         self.root = None
 
-    def add(self, key: str, value: Any) -> None:
+    def add(self, key: str, value: Any) -> bool:
+        node = Node(Key, value)
         if self.root is None:
-            self.root = Node(key, value)
+            self.root = node
+            return True
+
+        tmp = self.root
+        #if  tmp is prev's left child ,then True
+        left = None
+        while tmp is None:
+            prev = tmp
+            if tmp.key == key:
+                # key is alerady used
+                print("key is already used")
+                return False
+            elif tmp.key > key:
+                tmp = tmp.left
+                left = True
+            else:
+                tmp = tmp.right
+                left = False
+
+        if left:
+            prev.left = node
+
         else:
-            print()
+            prev.right = node
+        return True
