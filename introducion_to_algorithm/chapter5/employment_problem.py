47c84c6a3d716526f53f1823386f83cd9dc3953e
biwajima-taro <taro.biwajima@gmail.com>
2020-02-13 17:17:15 +0900

    biased estimator

--- introducion_to_algorithm/chapter5/employment_problem.py
+++ introducion_to_algorithm/chapter5/employment_problem.py
@@ -1,4 +1,5 @@
 from typing import List
+import random
 
 
 class Person:
@@ -17,3 +18,22 @@ def fire_assistant(candidate_list: List[Person]) -> None:
         if ele.score > best:
             hired.add(ele)
             best = ele.score
+
+
+def biased_generator(prob: float) -> float:
+    num = random.uniform(0, 1)
+    #print(num)
+    if prob > num:
+        return 1
+    else:
+        return 0
+
+
+def unbiased_generator(prob:float):
+    tmp = [biased_generator(prob) for i in range(1000)]
+    #TODO:make this calculation more accurate,see effective python 2nd
+    estimate = sum(tmp)/len(tmp)
+    print(estimate)
+
+if __name__ == "__main__":
+    print(unbiased_generator(0.3))
