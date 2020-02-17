b56add7dc28f3ac2dd797a955893c95dd2d5c69f
biwajima-taro <taro.biwajima@gmail.com>
2020-02-17 15:04:24 +0900

    permute by cyclic

--- introducion_to_algorithm/chapter5/randomized_algorithm.py
+++ introducion_to_algorithm/chapter5/randomized_algorithm.py
@@ -21,16 +21,51 @@ def permute_by_sort(input_list: List) -> List:
     return [ele.value for ele in ele_list]
 
 
-def randomlize_in_place(input_list: List) -> List:
+def randomize_in_place(input_list: List) -> List:
     n = len(input_list)
     for i in range(n):
         tmp = input_list[i]
-        random_index=int(unifrom(0,n))
+        random_index = int(uniform(0, n))
         input_list[i] = input_list[random_index]
-        input_list[random_index]=tmp
+        input_list[random_index] = tmp
     return input_list
+
+
+def permute_by_cyclic(input_list: List):
+    print(f"{len(input_list)}")
+    n = len(input_list)
+    output_list = [None]*n
+    offset = int(uniform(0, n))
+    print(offset)
+    for i in range(n):
+        dest = i+offset
+        if dest >= n:
+            print(f"{dest} > {n}")
+            dest -= n
+        output_list[dest] = input_list[i]
+
+    return output_list
+
+
+def random_sample(m: int, n: int):
+    # TODO check appropriate data type for variable s
+    if m == 0:
+        return 0
+    else:
+        s = random_sample(m-1, n-1)
+        i = random_sample(1, n)
+        if i in s:
+            s = s.appned(n)
+        else:
+            s = s.appned(i)
+    return s
 
 
 
 if __name__ == "__main__":
-    print(permute_by_sort([3, 4, 2, 1, 6, 7, 76]))
+    print(permute_by_cyclic([1,3,4,5,10,12,3,4]))
+    
+    #print(permute_by_sort([3, 4, 2, 1, 6, 7, 76]))
+    
+    #for i in range(100):
+     #   print(randomize_in_place([3, 4, 5, 6, 7, 3, 2, 11, 33, 44, 123]))
