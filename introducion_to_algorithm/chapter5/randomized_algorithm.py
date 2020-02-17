9835289bc63e0cdf3a00a42ab90ca6a622c8c1fc
biwajima-taro <taro.biwajima@gmail.com>
2020-02-17 11:41:26 +0900

    randomize in place

--- introducion_to_algorithm/chapter5/randomized_algorithm.py
+++ introducion_to_algorithm/chapter5/randomized_algorithm.py
@@ -6,18 +6,30 @@ class Element:
     def __init__(self, value: Any, priority: int):
         self.value = value
         self.priority = priority
+
     def __str__(self):
         return f"value:{self.value},priotiry:{self.priority}"
 
 
-def permute_by_sort(input_list: List):
+def permute_by_sort(input_list: List) -> List:
     n = len(input_list)
-    tmp_list: List[Element] = []
+    ele_list: List[Element] = []
     for value in input_list:
-        tmp_list.append(Element(value, uniform(1, n**3)))
-    print(tmp_list)
-    tmp_list=sorted(tmp_list, key=lambda u: u.priority)
+        ele_list.append(Element(value, int(uniform(0, n**3))))
+    print(ele_list)
+    ele_list = sorted(ele_list, key=lambda u: u.priority)
     return [ele.value for ele in tmp_list]
+
+
+def randomlize_in_place(input_list: List) -> List:
+    n = len(input_list)
+    for i in range(n):
+        tmp = input_list[i]
+        random_index=int(unifrom(0,n))
+        input_list[i] = input_list[random_index]
+        input_list[random_index]=tmp
+    return input_list
+
 
 
 if __name__ == "__main__":
