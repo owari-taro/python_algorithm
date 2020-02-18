0180cdcaecef515fb5f77b24f41a857983ef0ccc
biwajima-taro <taro.biwajima@gmail.com>
2020-02-18 16:33:20 +0900

    quick sort

--- PythonAlgorithms/quick_sort.py
+++ PythonAlgorithms/quick_sort.py
@@ -16,3 +16,23 @@ def partition(input_list: MutableSequence) -> None:
             a[pl], a[pr] = a[pr], a[pl]
             pl += 1
             pr -= 1
+    return a
+
+
+def q_sort(a: MutableSequence, left: int, right: int) -> None:
+    pl = left
+    pr = right
+    x = a[(right+left)//2]
+    while pl <= pr:
+        while a[pl] < x:
+            pl += 1
+        while a[pr] > x:
+            pr -= 1
+        if pl <= pr:
+            a[pl], a[pr] = a[pr], a[pl]
+            pl += 1
+            pr -= 1
+    if left < pr:
+        q_sort(a, left, pr)
+    if pl < right:
+        q_sort(a, right, pl)
