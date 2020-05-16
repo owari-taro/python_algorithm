34a1ecb0662f1f534fedd60895228cd02891cd93
owari-taro <taro.biwajima@gmail.com>
2020-05-16 20:13:37 +0900

    binary search

--- INTRODUCTION_TO_DS/chapter5_search/binary_search_tree.py
+++ INTRODUCTION_TO_DS/chapter5_search/binary_search_tree.py
@@ -1,14 +1,22 @@
 from dataclasses import dataclass
 from typing import List
-
+from __future__ import annotations
 # TODO:rewrite with dataclass
 
 
+class Direction(Enum):
+    left = "left"
+    right = "right"
+
+
+@dataclass
 class Node:
-    def __init__(self, value, left: 'Node' = None, right: 'Node' = None):
-        self.value = value
-        self.left = left
-        self.right = right
+    value: float
+    left: Node = None
+    right: Node = None
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.left!r},{self.right!r})"
 
     def __str__(self):
         left = f'[{self.left.value}]' if self.left else '{}'
@@ -20,32 +28,28 @@ class BInarySearchTree:
     def __init__(self):
         self.nodes: List[Node] = []
 
-    def add_node(self, value):
+    def add_node(self, value: float):
         node = Node(value)
         if self.nodes:
             parent, direction = self.find_parent(value)
-            # TODO:rewrite with enum!!!!!!!
-            if direction == "right":
+            if direction == Direction.right:
                 parent.right = node
             else:
                 parent.left = node
-
         self.nodes.append(node)
 
     def find_parent(self, value):
-        current = self.nodes
-        while current:
-            if current.value == value:
-                raise ValueError()
-            if curremt.value < value:
-                prev = parent
-                direction = "right"
-                cufrent = parent.right
+        node = self.nodes[0]
+        while node:
+            prev = node
+            if node.value == value:
+                raise ValueError("alredy exists")
+            elif node.value > value:
+                direction = Direction.right
+                node = node.right
             else:
-                prev = parent
-                direction = "left"
-                current = parent.left
-
+                direction = Direction.left
+                node = node.right
         return prev, direction
 
 
