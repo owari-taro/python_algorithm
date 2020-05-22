b0c940f823fb5b685d215df784b500cbe85d6b61
owari-taro <taro.biwajima@gmail.com>
2020-05-22 09:45:37 +0900

    selection

--- introducion_to_algorithm/chapter2_start/selection_sort.py
+++ introducion_to_algorithm/chapter2_start/selection_sort.py
@@ -1,14 +1,10 @@
-from typing import List
+from typing import MutableSequence
 
 
-def selection_sort(input_list: List[float]):
+def selection_sort(input_list: MutableSequence):
     original = input_list.copy()
-    for i in range(0, len(input_list)-1):
-        tmp = input_list[i]
-        index_ = i
-        for j in range(i+1, len(input_list)):
-            if tmp > input_list[j]:
-                tmp = input_list[j]
-                index_ = j
-            input_list[index_] = input_list[i]
-            input_list[i] = tmp
+    n = len(input_list)
+    for j in range(i+1, n):
+        if input_list[j] < input_list[min]:
+            min = j
+    input_list[i], input_list[min] = input_list[min], input_list[i]
