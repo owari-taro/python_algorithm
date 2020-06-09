13815a3d6072b6c558f6883306d80194415711e8
biwajima-taro <taro.biwajima@gmail.com>
2020-06-09 14:24:27 +0900

    add method for red black tree

--- introducion_to_algorithm/chapter13_red_balck_tree/problem2_1.py
+++ introducion_to_algorithm/chapter13_red_balck_tree/problem2_1.py
@@ -1,10 +1,23 @@
 import attr
 from typing import Any
+from enum import Enum
+
+
+class Color(Enum):
+    """
+    each node color
+
+    Args:
+        Enum ([type]): [description]
+    """
+    BLACK = 0
+    RED = 1
 
 
 @attr.s
 class Node:
     key: Any = attr.ib()
+    color: Color = attr.ib()
     right: Node = attr.ib(default=None)
     left: Node = attr.ib(default=None)
     parent: Node = attr.ib(default=None)
@@ -32,3 +45,30 @@ class Tree:
         child.parent = partial_tree_root.parent
         child.right = partial_tree_root
         partial_tree_root.parent = child
+
+    def rb_insert(self, new_node: Node) -> None:
+        current = self.head
+        parent = None
+        while current is None:
+            parent = current
+            if new_node.key < current.key:
+                left = True
+                current = current.left
+            else:
+                left = False
+                current = current.right
+            new_node.parent = parent
+            if parent is None:
+                self.head = new_node
+            elif left:
+                parent.left=new_node
+            else:
+                parent.right=new_node
+            new_node.right=None
+            new_node.left=None
+            new_node.color=Color.RED
+            #rb_insertFIXUP
+            NotImplementedError
+
+    def rb_insert_fixup(self):
+        print()
