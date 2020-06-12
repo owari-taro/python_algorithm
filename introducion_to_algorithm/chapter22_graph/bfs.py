762e5c3fb0ad829b64e6549b0902b01910fa234b
owari-taro <taro.biwajima@gmail.com>
2020-06-12 23:47:39 +0900

    bfs

--- introducion_to_algorithm/chapter22_graph/bfs.py
+++ introducion_to_algorithm/chapter22_graph/bfs.py
@@ -1,8 +1,10 @@
+from __future__ import annotations
 from dataclasses import dataclass
 from enum import Enum
 from typing import List
 import math
-from __future__ import annotations
+from collections import deque
+import attr
 
 
 class Color(Enum):
@@ -11,18 +13,45 @@ class Color(Enum):
     GRAY = "GRAY"
 
 
-@dataclass
+@attr.s
 class Vertex:
-    color: Color
+    id_: int
+    color: Color = attr.ib()
     # represeting distance from root
-    predecessor: Vertex = None
-    distance: float = math.inf
+    distance: float = attr.ib(default=None)
+    predecessor: Vertex = attr.ib(default=None)
+    adjacent: List[Vertex] = attr.ib(default=None)
 
-    def __eq__(self,other):
-        return self.color
-    
+    def __eq__(self, other: Vertex):
+        return self.id_ == other.id_
+
+@attr.s
 class Graph:
-    def __init__(self,root,vertex_list):
-        self.root=root
-        self.vertex_list=vertex_list
-    
+    #def __init__(self, root, vertex_list: List[List[Vertex]]):
+        root:Vertex = attr.ib()
+        vertex_list: List[Vertex] = attr.ib()
+
+
+def bfs(graph: Graph) -> Graph:
+    # initialize
+    for vertex in graph.vertex_list:
+        if vertex == graph.root:
+            continue
+        vertex.color = Color.WHITE
+        vertex.distance=math.inf
+        vetex.predecessor=None
+
+    queue = deque([])
+    queue.append(graph.root)
+    while queue:
+        current = queue.popleft()
+        for adj in current.adjacent:
+            # white means note visited before
+            if adj.color == Color.WHITE:
+                adj.color = Color.GRAY
+                adj.distance = current.distance+1
+                adj.parrent = current
+                queue.append(adj)
+        # finish
+        current.color = Color.BLACK
+    return graph
