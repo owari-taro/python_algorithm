aaa306b43d3e26652abc1e8b0e7c5eff858cff61
owari-taro <taro.biwajima@gmail.com>
2020-06-20 23:32:08 +0900

    fix bug

--- introducion_to_algorithm/chapter15_dp/bottom_up_cut_rod.py
+++ introducion_to_algorithm/chapter15_dp/bottom_up_cut_rod.py
@@ -16,12 +16,20 @@ def bottom_up_cut_rod(price: List, size: int):
     -------
     [type]
         [description]
-    """    
-    #when size is 0,value is 0
+    """
+    import time
+    # when size is 0,value is 0
     memo = [0]
     for i in range(size):
         current = -math.inf
-        for j in range(i):
-            current = max(current, p[j+1]+memo[j])
-        memo[j+1] = current
+        for j in range(i+1):
+            current = max(current,  price[j+1]+memo[i-j])
+        memo.append(current)
+    print(memo)
     return memo[size]
+
+
+if __name__ == "__main__":
+    price = [0, 1, 5, 8, 9, 10]  # ,#8,9,10]
+    size = len(price)
+    bottom_up_cut_rod(price, size-1)
