dd8d7dd47fc138cac3f8da8228cd5aa23e88eab2
owari-taro <taro.biwajima@gmail.com>
2020-06-24 22:54:43 +0900

    add class method

--- introducion_to_algorithm/chapter15_dp/knapsack.py
+++ introducion_to_algorithm/chapter15_dp/knapsack.py
@@ -25,3 +25,15 @@ class Knapsack:
 
     def has_room_for(self, item):
         return self.size >= self.weight+item.weight
+
+    @classmethod
+    def greedy(cls, items: List[Item], size_limit: flaot):
+        sorted_items: List[Item] = sorted(
+            items, key=lambda: x.price/x.weight, reverse=True)
+        knapsack = cls(size_limit)
+        for item in sorted_items:
+            try:
+                knapsack.append(item)
+            except ValueError:
+                continue
+        return knapsack
