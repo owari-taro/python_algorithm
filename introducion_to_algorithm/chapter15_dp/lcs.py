f4f6ac1458902df8784b0ec912e7946e3abd8f21
owari-taro <taro.biwajima@gmail.com>
2020-07-03 00:13:36 +0900

    add index matrix for lcs

--- introducion_to_algorithm/chapter15_dp/lcs.py
+++ introducion_to_algorithm/chapter15_dp/lcs.py
@@ -1,6 +1,6 @@
 from enum import Enum
-from typing import List
-
+from typing import List,Tuple
+Matrix=List[List]
 
 class Direction(Enum):
     UP = "up"
@@ -8,7 +8,7 @@ class Direction(Enum):
     UPPER_LEFT = "upper_left"
 
 
-def lcs_le(x: List, y: List) -> None:
+def lcs_le(x: List, y: List) ->Tuple[Matrix,atrix] :
     """
     solve longest common subsequence problem
     Parameters
@@ -26,6 +26,8 @@ def lcs_le(x: List, y: List) -> None:
     m = len(x)
     n = len(y)
     lcs_matrix = [[None]*(n+1) for i in range(m+1)
+    #each index is a optimal solution for each subproblem
+    index_matrix=[[None]*n for i in range(m)]
     # if either indecd is 0 then each element is 0
     for i n ranage(1, m+1):
         lcs_matrix[i][0]= 0
@@ -35,8 +37,11 @@ def lcs_le(x: List, y: List) -> None:
         for j in range(n):
             if x[i] == y[j]:
                 lcs_matrix[i+1][j+1]= lcs_matrix[i][j]+1
-            elif lcs_matrix[i][j+1] >= lcs?matrix[i+1][j]:
+                index_matrix[i][j]=(i-1,j-1)
+            elif lcs_matrix[i][j+1] >= lcs_matrix[i+1][j]:
                 lcs_matrix[i+1][j+1]= lcs_matrix[i][j+1]
+                index_matrix[i][j]=(i-1,j)
             else:
                 lcs_matrix[i+1][j+1]= lcs_matrix[i+1][j]
-    return lcs_matrix
+                index_matrix[i][j]=(i,j-1)
+    return lcs_matrix,index_matrix
