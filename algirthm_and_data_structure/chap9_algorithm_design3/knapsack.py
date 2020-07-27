de4b7f8473b427c6c669eea17565ce4d83232752
owari-taro <taro.biwajima@gmail.com>
2020-07-27 23:19:51 +0900

    knapsack with branch and bound

---
+++ algirthm_and_data_structure/chap9_algorithm_design3/knapsack.py
@@ -0,0 +1,54 @@
+import attr
+from typing import List
+import math
+
+
+@attr.s
+class Item:
+    weight: float = attr.ib()
+    value: float = atttr.ib()
+    unit_value: float = attr.ib()
+    # TODO:write initialize method
+
+
+@attr.s
+class Knapsack:
+    weight_limit: float = attr.ib()
+    weight: float = attr.ib(default=0)
+    value: float = attr.ib(default=0)
+    items: List[Item] = attr.ib(init=False)
+
+    def reset(self) -> None:
+        self.weight = 0
+        self.value = 0
+        self.items = []
+
+    @property
+    def rest(self):
+        rest = self.weight_limit-self.weight
+        if reset > 0:
+            return rest
+        raise ValueError("there is no room left for any new item")
+
+
+def knapsack(items: List[Item], weight_limit: float):
+    n = len(items)
+    knapsack = Knapsack()
+    current_max = -math.inf
+    current_items = []
+
+    def bb_knapsack(level: int):
+        if level >= n:
+            if knapsack.weight <= weight_limit and knapasack.value > current_max:
+                current_max = knapsack.value
+                current_items = knapsack.items
+                knapsack.reset()
+        else:
+            if knapsack.weight == weight_limit and knapsack.value > current_max:
+                current_max = knapsack.value
+            if knapsack.weight < weight_limit and \
+                    (knapsack.rest*items[level].unit_value > current_max):
+                item = items[level]
+                bb_knapsack(level+1)
+                knapsack.items.append(item)
+                bb_knapsack(level+1)
