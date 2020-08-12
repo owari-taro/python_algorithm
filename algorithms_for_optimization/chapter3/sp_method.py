c59314cd93dd183e9499ca8023845246683c1a7a
owari-taro <taro.biwajima@gmail.com>
2020-08-12 21:54:44 +0900

    sp method at Point

--- algorithms_for_optimization/chapter3/sp_method.py
+++ algorithms_for_optimization/chapter3/sp_method.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 from dataclasses import dataclass
-
+from  typing import Callable
 
 @dataclass
 class Point:
@@ -10,4 +10,27 @@ class Point:
     @classmethod
     def get_sp_intersection(self, A: Point, b: Point, slope: float) -> Point:
         intersection = ((A.y-B.y)-slope*(A.x-B.x))/2*slope
-        return Point(A.x+intersection, A.y+intersection*slope)
+        return Point(A.x+intersection, A.y+intersection*slope)  
+    
+    def shubert_piyavskii(self,f:Callable,a:Point,b:Point,slope:float,eps:float,delat:float=0.01):
+        """
+        [summary]
+
+        Parameters
+        ----------
+        f : Callable
+            [description]
+        a : Point
+            [description]
+        b : Point
+            [description]
+        slope : float
+            [description]
+        eps : float
+            [description]
+        delat : float, optional
+            [description], by default 0.01
+        """        
+        m=(a+b)/2
+        A,B,M=Point(a,f(a)),Point(b,f(b)),Point(m,f(m))
+        #TODO:implement below
